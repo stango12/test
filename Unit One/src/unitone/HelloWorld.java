7004e66690d295f3e871d3299f70894f56869fc4
157845 <157845@HSS-101-14.westwindsor.k12.nj.us>
2013-12-20 08:51:50 -0500

    initial commit

---
+++ Unit One/src/unitone/HelloWorld.java
@@ -0,0 +1,29 @@
+package unitone;
+
+public class HelloWorld {
+
+	/**
+	 * Mr. Smith(Steven Tang)
+	 * My first program
+	 * 9/4/13
+	 * 
+	 * Hello World!
+	 * this is a multi-line comment section used for name, date, 
+	 * program description, notes etc...
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+		// print to the console
+		System.out.println("Hello World! ");
+		System.out.println("My name is Steven.");
+		System.out.println("This is fun");
+		
+		
+		//learning about tabbing with escape sequences, good for columns
+		// \n = new line. \t = one tab 
+		System.out.println("\n\n\tPer 3\t\tComputer Programming");
+		
+	}
+
+}
---
+++ Unit One/src/unitone/MySchedule.java
@@ -0,0 +1,31 @@
+package unitone;
+
+public class MySchedule {
+
+	/**
+	 * Printing my schedule in a nicely formatted and in 3 labeled columns
+	 * Mr. Smith
+	 * 9/4
+	 * 
+	 * like
+	 * Per		Course		Teacher
+	 * Per 1	Bio			Jones
+	 */
+	public static void main(String[] args) {
+		
+		// print out schedule
+		System.out.println("Per \t\t Course \t\t Teacher");
+		System.out.println("-------------------------------------------------------------");
+		
+		System.out.println("Per 1 \t\t Study Hall \t\t None");
+		System.out.println("Per 2 \t\t Physics \t\t Todd");
+		System.out.println("Per 3 \t\t Computer Programming \t Smith");
+		System.out.println("Per 4 \t\t Stat \t\t\t Brienza");
+		System.out.println("Per 5 \t\t Orchestra \t\t Mauro");
+		System.out.println("Per 6 \t\t Independent Study \t Ashton");
+		System.out.println("Per 7 \t\t Gender Roles \t\t Bonora");
+		System.out.println("Per 8 \t\t Health \t\t Miller");
+	
+	}
+
+}
