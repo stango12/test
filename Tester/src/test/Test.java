ebbef160baf9759cfe9c64044bf9b55fec5e43eb
157845 <157845@HSS-101-19.westwindsor.k12.nj.us>
2014-02-25 08:59:39 -0500

    test

--- Tester/src/test/Test.java
+++ Tester/src/test/Test.java
@@ -1,5 +1,14 @@
 package test;
 
-public class Test {
+import java.awt.Color;
 
+public class Test 
+{
+	public static void main(String[] args)
+	{
+		Color test = Color.ORANGE;
+		System.out.println(test);
+		System.out.println(test.darker());
+		System.out.println(test.brighter());
+	}
 }
